7fe57dd16e1f66d059b52c78e067e5e53b4eca38
Michael Biebl <biebl@debian.org>
2018-07-03 23:59:36 +0200

    New upstream version 1.8.14

--- src/libnma/nma-version.h
+++ src/libnma/nma-version.h
@@ -45,7 +45,7 @@
  * Evaluates to the micro version number of NetworkManager which this source
  * compiled against.
  */
-#define NMA_MICRO_VERSION (12)
+#define NMA_MICRO_VERSION (14)
 
 /**
  * NMA_CHECK_VERSION:
